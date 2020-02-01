5d7ee234017d55b07bd2559306115969e8a03744
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2020-02-01 03:14:25 +0200

    update

--- app2/components/Posts/PostList.vue
+++ app2/components/Posts/PostList.vue
@@ -2,16 +2,19 @@
   <section class="post-list">
       <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://i.pinimg.com/originals/74/22/87/742287c1114542dd95e2befa3e653c06.png"
       title="Hello there"
       previewText = "This is my first post!" />
       <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://i.pinimg.com/originals/74/22/87/742287c1114542dd95e2befa3e653c06.png"
       title="Hello there"
       previewText = "This is my post no!" />
       <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://i.pinimg.com/originals/74/22/87/742287c1114542dd95e2befa3e653c06.png"
       title="Hello there"
       previewText = "This is my 3 post!" />
@@ -24,8 +27,13 @@ import PostPreview from '@/components/Posts/PostPreview'
 export default {
     components: {
         PostPreview
-    }
-    
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
+    },    
 }
 </script>
 
